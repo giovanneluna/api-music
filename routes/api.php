876bb0c514bf21ef37556db073a5cddfc946b9bf
Giovanne de Luna <gikalunalticg@gmail.com>
2025-04-04 12:44:20 -0300

    feat: prefixo /api para todas as rotas
    Configura todas as rotas da API para utilizarem o prefixo /api, garantindo uma estrutura RESTful padronizada e seguindo boas práticas.

--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MusicController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SuggestionController;
+use App\Http\Controllers\SuggestionStatusController;
 
-
-Route::get('/', function (Request $request) {
-    return response()->json(['message' => 'API funcionando!']);
-});
-
-Route::middleware('api')->prefix('auth')->group(function () {
+Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'user']);
     Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 });
 
+Route::apiResource('musics', MusicController::class);
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [AuthController::class, 'user']);
+    Route::apiResource('suggestions', SuggestionController::class);
+
+    Route::post(
+        'suggestions/{suggestion}/status/{status}',
+        SuggestionStatusController::class
+    );
 });
